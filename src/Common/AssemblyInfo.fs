fce147cdc23df42d83d2c2afcb10006000634244
Dmitriy Vlasov <vlasovde@outlook.com>
2015-03-27 01:58:02 +0300

    Add Assemblyinfo

---
+++ src/Common/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Common")>]
+[<assembly: AssemblyProductAttribute("Common")>]
+[<assembly: AssemblyDescriptionAttribute("My collection of utilities on FSharp.")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
